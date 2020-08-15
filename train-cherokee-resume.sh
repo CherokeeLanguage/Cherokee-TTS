25e3bdcc9b2da47b0793d2ad42ec1f10becc1521
Michael Conrad <m.conrad.202@gmail.com>
2020-08-15 07:21:01 -0400

    training script adjustments

---
+++ train-cherokee-resume.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+set -e
+set -o pipefail
+clear
+
+cd "$(dirname "$0")"
+
+cp="$(ls -1tr checkpoints/|tail -n 1)"
+cp="$(basename "$cp")"
+
+printf "Using checkpoint: $cp\n"
+
+date
+
+export PYTHONIOENCODING=utf-8
+python train-ga.py --checkpoint "$cp" --accumulation_size 10
+
+date
--- train-cherokee.sh
+++ train-cherokee.sh
@@ -3,4 +3,9 @@ set -e
 set -o pipefail
 clear
 
+date
+
+export PYTHONIOENCODING=utf-8
 python train-ga.py --hyper_parameters generated_switching_cherokee --accumulation_size 10
+
+date
