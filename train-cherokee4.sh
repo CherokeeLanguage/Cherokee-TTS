5e0dd1f5a97dd477d36766e0dd287788b86a16aa
Michael Conrad <m.conrad.202@gmail.com>
2020-08-18 12:04:50 -0400

    ensure spectrograms exist

--- train-cherokee4.sh
+++ train-cherokee4.sh
@@ -3,10 +3,15 @@ set -e
 set -o pipefail
 clear
 
+cd "$(dirname "$0")"
+WORK="$(pwd)"
+cd data
+python prepare_cherokee4_spectrograms.py
+
 date
 
+cd "$WORK"
 export PYTHONIOENCODING=utf-8
-
 python train-ga.py --hyper_parameters generated_switching_cherokee4 --accumulation_size 2
 
 date
