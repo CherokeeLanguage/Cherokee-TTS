7fb7386047004f95a22e4a2275306d14e370baa3
Michael Conrad <m.conrad.202@gmail.com>
2020-08-24 16:31:36 -0400

    add training data

--- data/cherokee-audio/sam-hider/generate-training-data-1.py
+++ data/cherokee-audio/sam-hider/generate-training-data-1.py
@@ -13,14 +13,21 @@ from md2pdf.core import md2pdf
 
 os.chdir(os.path.dirname(sys.argv[0]))
 
-rmtree("wav", ignore_errors=True)
+MASTER_TEXT:str="sam-hider.txt"
+
+#cleanup any previous runs
+for dir in ["linear_spectrograms", "spectrograms", "wav"]:
+    rmtree(dir, ignore_errors=True)
+    
 pathlib.Path(".").joinpath("wav").mkdir(exist_ok=True)
 
-with open("sam-hider.txt", "r") as f:
+with open(MASTER_TEXT, "r") as f:
     entries: list = []
     for line in f:
         mp3: str=line.split("|")[0].strip()
         text: str=ud.normalize("NFD", line.split("|")[1].strip())
+        if text=="":
+            continue
         entries.append((mp3,text))
 
 voiceid:str=""
@@ -29,16 +36,14 @@ with open("voice-id.txt", "r") as f:
         voiceid=line.strip()
         break
 
-id:int=120000
+id:int=10000 * int(re.sub("^(\d+).*", "\\1",voiceid))
 
-FNULL = open(os.devnull, 'w')
 rows:list=[]
 for mp3, text in entries:
     wav:str="wav/"+os.path.splitext(os.path.basename(mp3))[0]+".wav"
     text:str=ud.normalize('NFC', text)
-    subprocess.run(["ffmpeg","-i",mp3,"-ac","1","-ar","22050",wav], check=True, stdout=FNULL, stderr=FNULL)
-    # 100201|01-chr|chr|../cherokee/ma-split/Track_45-006.wav|../cherokee/spectrograms/100201.npy|../cherokee/linear_spectrograms/100201.npy|sǔ:dáli.|
-    rows.append(f"{id:06d}|{voiceid}|chr|../../cherokee-audio/sam-hider/{wav}|||{text}|")
+    subprocess.run(["ffmpeg","-i",mp3,"-ac","1","-ar","22050",wav], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    rows.append(f"{id:06d}|{voiceid}|chr|{wav}|||{text}|")
     id+=1
 
 random.Random(id).shuffle(rows)
