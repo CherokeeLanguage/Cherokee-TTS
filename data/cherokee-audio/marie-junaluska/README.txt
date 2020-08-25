e9125d5df2158e898445ac649c6c6a4fa0ecc44f
Michael Conrad <m.conrad.202@gmail.com>
2020-08-25 16:44:08 -0400

    Marie Junaluska training data pulled from CD-ROM

---
+++ data/cherokee-audio/marie-junaluska/README.txt
@@ -0,0 +1,11 @@
+#Marie Junaluska
+
+##Audio Training Data
+
+Audio Copyright: Standard, US.
+
+The audio data comes from the CD-ROM "Cherokee Kituwah Language Sampler".
+
+### Notes
+
+* The extracted audio data is NOT available for download and will NOT be redistributed. (Copyright reasons.)
---
+++ data/cherokee-audio/marie-junaluska/generate-training-data-1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import os
+import sys
+import string
+import unicodedata as ud
+import random
+import re
+import pathlib
+import subprocess
+from shutil import rmtree
+
+from md2pdf.core import md2pdf
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+MASTER_TEXT:str="sam-hider.txt"
+
+#cleanup any previous runs
+for dir in ["linear_spectrograms", "spectrograms", "wav"]:
+    rmtree(dir, ignore_errors=True)
+    
+pathlib.Path(".").joinpath("wav").mkdir(exist_ok=True)
+
+with open(MASTER_TEXT, "r") as f:
+    entries: list = []
+    for line in f:
+        mp3: str=line.split("|")[0].strip()
+        text: str=ud.normalize("NFD", line.split("|")[1].strip())
+        if text=="":
+            continue
+        entries.append((mp3,text))
+
+voiceid:str=""
+with open("voice-id.txt", "r") as f:
+    for line in f:
+        voiceid=line.strip()
+        break
+
+id:int=10000 * int(re.sub("^(\d+).*", "\\1",voiceid))
+
+rows:list=[]
+for mp3, text in entries:
+    wav:str="wav/"+os.path.splitext(os.path.basename(mp3))[0]+".wav"
+    text:str=ud.normalize('NFC', text)
+    subprocess.run(["ffmpeg","-i",mp3,"-ac","1","-ar","22050",wav], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    rows.append(f"{id:06d}|{voiceid}|chr|{wav}|||{text}|")
+    id+=1
+
+random.Random(id).shuffle(rows)
+trainSize:int=(int)(len(rows)*.95)
+with open("train.txt", "w") as f:
+    for line in rows[:trainSize]:
+        f.write(line)
+        f.write("\n")
+        
+with open("val.txt", "w") as f:
+    for line in rows[trainSize:]:
+        f.write(line)
+        f.write("\n")
+
+sys.exit()
