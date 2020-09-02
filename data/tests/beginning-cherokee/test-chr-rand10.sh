d3aeef486bdc4525e66bd76349407a9b1e9261f0
Michael Conrad <m.conrad.202@gmail.com>
2020-09-01 22:55:53 -0400

    Use beginning cherokee data for tests

---
+++ data/tests/beginning-cherokee/test-chr-rand10.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+cd "$(dirname "$0")"
+z="$(pwd)"
+
+rm *.npy 2> /dev/null || true
+rm *.wav 2> /dev/null || true
+
+for x in "$z"/*-[0-9][0-9]-*; do
+	if [ ! -d "$x" ]; then continue; fi
+	rm -r "$x"
+done
+
+cd ../../..
+y="$(pwd)"
+
+source ~/miniconda3/etc/profile.d/conda.sh
+
+conda activate ./env
+
+cp="$(ls -1tr checkpoints/*CHEROKEE*|tail -n 1)"
+cp="$(basename "$cp")"
+
+printf "Using checkpoint: $cp\n"
+
+tmp="$z/tmp.txt"
+selected="$z/selected.txt"
+cp /dev/null "$tmp"
+
+cp /dev/null "$z"/voices.txt
+
+wg="bc"
+for x in "$z"/"$wg"-[0-9][0-9]-*; do
+	if [ ! -d "$x" ]; then continue; fi
+	rm -r "$x"
+done
+
+v=("01-chr" "02-chr" "03-chr" "04-chr")
+vsize="${#v[@]}"
+
+printf "\nTotal voice count: %d\n\n" "$vsize"
+
+text="$z/../../cherokee-audio/beginning-cherokee/val.txt"
+
+cut -d '|' -f 7 "$text" | shuf -n 50 | shuf -n 25 | shuf -n 10 | sort > "$selected"
+
+for voice in "${v[@]}"; do
+	printf "Generating audio for %s\n" "$voice"
+	ix=0
+	syn=""
+	cp /dev/null "$tmp"
+	cat "$selected" | while read phrase; do
+		ix=$(($ix+1))
+		printf "%d|%s|%s|chr\n" "$ix" "${phrase}" "$voice" >> "$tmp"
+	done
+
+	cd "$y"
+	
+	cat "$tmp" | python synthesize.py --output "$z/" --save_spec --checkpoint "checkpoints/$cp" #--cpu
+
+	cd "$z"
+	
+	rm -r "$wg"-"$voice" 2> /dev/null || true
+	mkdir "$wg"-"$voice"
+	cp -p "$selected" "$wg"-"$voice"
+	
+	#python wavernnx-cpu.py
+	python wavernnx.py
+
+	count=$(wc -l "$selected"|cut -f 1 -d ' ')
+	for ix in $(seq 1 $count); do
+		iy=$(printf "%02d" $ix)
+		wav="$wg"-"$voice/wg-$ix.wav"
+		mv "wg-$ix.wav" "$wav"
+		mp3="$wg"-"$voice/$wg-$voice-$iy".mp3
+		ffmpeg -i "$wav" -codec:a libmp3lame -qscale:a 4 "$mp3" > /dev/null 2> /dev/null
+		rm "$wav"
+	done
+	
+	xdg-open "$wg"-"$voice"
+	
+	count=$(wc -l "$selected"|cut -f 1 -d ' ')
+	for ix in $(seq 1 $count); do
+		rm "$ix".wav 2> /dev/null || true
+		rm "$ix".npy 2> /dev/null || true
+	done
+	printf "\n\n"
+done
