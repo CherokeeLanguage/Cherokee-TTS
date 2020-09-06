c893d179f3920ba897f682fa14b9016c55d046e0
Michael Conrad <m.conrad.202@gmail.com>
2020-09-06 10:56:32 -0400

    Multiple transcript fixes to deal with aeneas extraction mis-alignments

--- data/cherokee-audio/beginning-cherokee/split-aeneus.py
+++ data/cherokee-audio/beginning-cherokee/split-aeneus.py
@@ -95,9 +95,11 @@ if __name__ == "__main__":
             for line in f:
                 tLines.append(line.strip())
         with open("espeak.txt", "w") as f:
+            i:int=0
             for line in tLines:
+                i+=1
                 espeak:str=mco2espeak(line)
-                revEspeakLookup[espeak]=line
+                revEspeakLookup[i]=line
                 f.write(espeak)
                 f.write("\n")
         
@@ -111,11 +113,11 @@ if __name__ == "__main__":
         config +="|"+RuntimeConfiguration.MFCC_MASK_NONSPEECH_L3+"=True"
 #        config +="|os_task_file_levels=123"
         
-        config +="|task_adjust_boundary_nonspeech_min=0.1"
+        config +="|task_adjust_boundary_nonspeech_min=0.05"
         config +="|task_adjust_boundary_nonspeech_string=(sil)"
         
-        config +="|is_audio_file_detect_head_max=3.0"
-        config +="|is_audio_file_detect_tail_max=3.0"
+        config +="|is_audio_file_detect_head_max=1.5"
+        config +="|is_audio_file_detect_tail_max=1.5"
         
         task:Task = Task(config_string=config)
         task.audio_file_path_absolute=mp3
@@ -133,20 +135,17 @@ if __name__ == "__main__":
                 if fragment["id"][0]=="n":
                     continue
                 i+=1
+                text:str=""
+                text=revEspeakLookup[i]
+                if text[0] == "#":
+                    continue
                 begin=max(0,int((float(fragment["begin"])-.1)*1000)) #milliseconds
                 end=min(len(audioData),max(0,int((float(fragment["end"])+.2)*1000))) #milliseconds
                 segment=audioData[begin:end]
                 segment=match_target_amplitude(segment, -16.0)
                 segment.export(f"mp3-aligned/{os.path.basename(mp3)}-{i:03d}.mp3",bitrate="192k",format="mp3")
                 splits.append(f"mp3-aligned/{os.path.basename(mp3)}-{i:03d}.mp3")
-                phrases:list=fragment["lines"]
-                text:str=""
-                for phrase in phrases:
-                    tmp=revEspeakLookup[phrase]
-                    if len(text)>0:
-                        text+=" "
-                    text+=tmp
-                with open("ma-aeneas.txt", "a") as f:
+                with open("aeneas.txt", "a") as f:
                     f.write("?") #speaker
                     f.write("|")
                     f.write(f"mp3-aligned/{os.path.basename(mp3)}-{i:03d}.mp3") #audio file
