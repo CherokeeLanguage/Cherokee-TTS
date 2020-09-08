57481a248ff800306cfbee9e841a5a204adb052b
Michael Conrad <m.conrad.202@gmail.com>
2020-09-08 10:19:02 -0400

    script fix

--- train-cherokee6f-resume-reset-epochs.sh
+++ train-cherokee6f-resume-reset-epochs.sh
@@ -5,7 +5,8 @@ clear
 
 params="cherokee6f"
 #cp="GENERATED-SWITCHING-CHEROKEE6F_loss-44-0.226"
-cp="GENERATED-SWITCHING-CHEROKEE6F_loss-52-0.213"
+#cp="GENERATED-SWITCHING-CHEROKEE6F_loss-52-0.213"
+cp="GENERATED-SWITCHING-CHEROKEE6F_loss-64-0.181"
 
 cd "$(dirname "$0")"
 WORK="$(pwd)"
@@ -17,6 +18,6 @@ date
 
 cd "$WORK"
 export PYTHONIOENCODING=utf-8
-python train-ga.py --accumulation_size 4 --checkpoint "$cp"
+python trainGa.py --reset_epoch True --accumulation_size 4 --checkpoint "$cp"
 
 date
