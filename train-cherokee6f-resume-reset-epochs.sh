90c8d9e3bd3056ef4ea7080b477731f7a5745a6e
Michael Conrad <m.conrad.202@gmail.com>
2020-09-08 08:08:34 -0400

    prep to restart training with existing model with additional data

---
+++ train-cherokee6f-resume-reset-epochs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+set -e
+set -o pipefail
+clear
+
+params="cherokee6f"
+#cp="GENERATED-SWITCHING-CHEROKEE6F_loss-44-0.226"
+cp="GENERATED-SWITCHING-CHEROKEE6F_loss-52-0.213"
+
+cd "$(dirname "$0")"
+WORK="$(pwd)"
+
+cd data
+python prepare_spectrograms.py --directory "$params"
+
+date
+
+cd "$WORK"
+export PYTHONIOENCODING=utf-8
+python train-ga.py --accumulation_size 4 --checkpoint "$cp"
+
+date
