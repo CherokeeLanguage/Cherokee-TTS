7b2c4182d63ac43e45513de4adf2293915ab3bda
Michael Conrad <m.conrad.202@gmail.com>
2020-10-04 15:53:34 -0400

    restart 6i with cleaned up data

---
+++ train-cherokee6i-02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+set -e
+set -o pipefail
+clear
+
+params="cherokee6i"
+cp="cherokee6i_loss-192-0.216"
+
+cd "$(dirname "$0")"
+WORK="$(pwd)"
+
+cp data/"$params"/checkpoint/"$cp" checkpoints/
+
+cd data
+python prepare_spectrograms.py --directory "$params"
+
+date
+
+cd "$WORK"
+export PYTHONIOENCODING=utf-8
+python trainGa.py --logging_start 0 --reset_epoch True --accumulation_size 5 --checkpoint "$cp"
+
+date
