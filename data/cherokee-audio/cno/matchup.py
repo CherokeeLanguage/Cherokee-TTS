bc311b5f69a2a7ef2eeee4552665b34c73351325
Michael Conrad <m.conrad.202@gmail.com>
2020-10-31 09:38:14 -0400

    adjustment to matches

--- data/cherokee-audio/cno/matchup.py
+++ data/cherokee-audio/cno/matchup.py
@@ -5,6 +5,10 @@ if __name__ == "__main__":
     import sys
     import csv
     import codecs
+    import re
+    
+    from chrutils import ascii_ced2mco
+    from chrutils import ascii_ced2ced
     
     dname=os.path.dirname(sys.argv[0])
     if len(dname)>0:
@@ -20,6 +24,7 @@ if __name__ == "__main__":
     ced_lookup:dict=dict()
     rrd_lookup:dict=dict()
     cno_lookup:dict=dict()
+    mp3_lookup:dict=dict()
     
     ambig:dict=dict()
     
@@ -66,7 +71,10 @@ if __name__ == "__main__":
                     ambig[value]=True
                     rrd_lookup.pop(value)
                     continue
-                
+                #convert RRD to CED formatting
+                pronounce=re.sub("(?i)([aeiouv])([a-zɂ?])", "\\g<1>2\\g<2>", pronounce, flags=re.IGNORECASE)
+                pronounce=re.sub("(?i)([ạẹịọụ])([a-zɂ?])", "\\g<1>2\\g<2>", pronounce, flags=re.IGNORECASE)
+                pronounce=re.sub("(?i)(ṿ)([a-zɂ?])", "\\g<1>2\\g<2>", pronounce, flags=re.IGNORECASE)
                 rrd_lookup[value]=pronounce
                 
     print(f"Skipped loading {len(ambig):,} ambiguous entries from main dictionary files")
@@ -86,22 +94,30 @@ if __name__ == "__main__":
                     continue
                 if value in ced_lookup.keys():
                     cno_lookup[value]=ced_lookup[value]
+                    mp3_lookup[value]=record["notes"]
                     continue
                 if value in rrd_lookup.keys():
                     cno_lookup[value]=rrd_lookup[value]
+                    mp3_lookup[value]=record["notes"]
                     continue
                 if value+"Ꭲ" in ambig.keys():
                     continue
                 if value+"Ꭲ" in ced_lookup.keys():
                     cno_lookup[value]=ced_lookup[value+"Ꭲ"]
+                    mp3_lookup[value]=record["notes"]
                     continue
                 if value+"Ꭲ" in rrd_lookup.keys():
                     cno_lookup[value]=rrd_lookup[value+"Ꭲ"]
+                    mp3_lookup[value]=record["notes"]
                     continue                            
 
         print(f"Found {len(cno_lookup)} matches.")
         
     with open("matches.txt", "w") as file:
         for key in cno_lookup.keys():
-            print(f"{key}|{cno_lookup[key]}",file=file)
+            pronounce=cno_lookup[key]
+            mp3=mp3_lookup[key].replace("https://data.cherokee.org/Cherokee/LexiconSoundFiles/", "")
+            mco=ascii_ced2mco(pronounce)
+            pced=ascii_ced2ced(pronounce)
+            print(f"{key}|{mco}|{mp3}|{pronounce}|{pced}",file=file)
         
