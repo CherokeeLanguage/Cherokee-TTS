ffefa565072615ff037ca30ab1ad80d8c5c091f8
Michael Conrad <m.conrad.202@gmail.com>
2020-11-16 08:06:12 -0500

    fix wavernn source npy path

--- data/tests/ced-limited-multi-word/generate-mp3-audio-1.sh
+++ data/tests/ced-limited-multi-word/generate-mp3-audio-1.sh
@@ -3,8 +3,6 @@
 set -e
 set -o pipefail
 
-trap "echo ERROR" ERR
-
 cd "$(dirname "$0")"
 z="$(pwd)"
 
@@ -14,40 +12,41 @@ rm *.wav 2> /dev/null || true
 cd ../../..
 y="$(pwd)"
 
-conda activate ./env
+conda activate Cherokee-TTS
 
-#cp="$(ls -1tr checkpoints/*CHEROKEE4*|tail -n 1)"
-cp="$(ls -1tr checkpoints/*CHEROKEE*|tail -n 1)"
+cp="$(ls -1tr checkpoints/*|tail -n 1)"
 cp="$(basename "$cp")"
 
 printf "Using checkpoint: $cp\n"
 
-selected="$z/selected.txt"
-
 tmp="$z/tmp.txt"
+selected="$z/for-audio-quality-vote.txt"
 cp /dev/null "$tmp"
 
-for x in "$z"/flat-[0-9][0-9]-*; do
+cp /dev/null "$z"/voices.txt
+
+wg="ced-limited-multi-word-1"
+for x in "$z"/"$wg"-*; do
 	if [ ! -d "$x" ]; then continue; fi
 	rm -r "$x"
 done
 
-v=("cno-spk_0" "cno-spk_1" "cno-spk_2" "cno-spk_3" "09-chr" "08-chr" "05-chr" "04-chr" "03-chr" "02-chr" "01-chr")
+v=("cno-spk_0" "cno-spk_1" "cno-spk_2" "cno-spk_3")
+
 vsize="${#v[@]}"
 
 printf "\nTotal voice count: %d\n\n" "$vsize"
 
-wg="ced-limited-multi-word"
-text="$z/../../cherokee-ced-limited-multi-word/scripts/script-bare-1.txt"
+text="$z/scripts/script-bare-1.txt"
 
-cat "$text" > "$selected"
+cat "$text" | sort > "$selected"
 
 for voice in "${v[@]}"; do
 	printf "Generating audio for %s\n" "$voice"
-	ix=0
 	syn=""
 	cp /dev/null "$tmp"
-	cat "$selected" | while read phrase; do
+	ix=0
+	cat "$selected" | cut -f 1 -d '|' | while read phrase; do
 		ix=$(($ix+1))
 		printf "%d|%s|%s|chr\n" "$ix" "${phrase}" "$voice" >> "$tmp"
 	done
@@ -62,25 +61,27 @@ for voice in "${v[@]}"; do
 	mkdir "$wg"-"$voice"
 	cp -p "$selected" "$wg"-"$voice"
 	
-	#python wavernnx-cpu.py
-	python wavernnx.py
-	
+	python wavernnx.py || python wavernnx-cpu.py
 
-	count=$(wc -l "$selected"|cut -f 1 -d ' ')
-	for ix in $(seq 1 $count); do
+	ix=0
+	cat "$selected" | while read phrase; do
+		ix=$(($ix+1))
 		iy=$(printf "%02d" $ix)
 		wav="$wg"-"$voice/wg-$ix.wav"
-		mv "$ix.wav" "$wav"
+		mv "wg-$ix.wav" "$wav"
 		mp3="$wg"-"$voice/$wg-$voice-$iy".mp3
-		ffmpeg -i "$wav" -codec:a libmp3lame -qscale:a 4 "$mp3" 2>&1 > /dev/null
+		txt="$wg"-"$voice/$wg-$voice-$iy".txt
+		echo "$phrase" > "$txt"
+		normalize-audio -q "$wav"
+		ffmpeg -y -i "$wav" -codec:a libmp3lame -qscale:a 4 "$mp3" > /dev/null 2> /dev/null < /dev/null
 		rm "$wav"
 	done
 	
 	xdg-open "$wg"-"$voice"
 	
 	ix=0
-	cat "$selected" | while read line; do
-		ix="$(($ix+1))"
+	cat "$selected" | while read phrase; do
+		ix=$(($ix+1))
 		rm "$ix".wav 2> /dev/null || true
 		rm "$ix".npy 2> /dev/null || true
 	done
