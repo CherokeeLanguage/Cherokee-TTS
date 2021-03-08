52b3c7802055bac07f2778569dccadc4f075c3f8
Michael Conrad <m.conrad.202@gmail.com>
2021-03-07 23:02:10 -0500

    xxx

--- tts-wrapper/test.sh
+++ tts-wrapper/test.sh
@@ -3,8 +3,10 @@
 set -e
 set -o pipefail
 
+CP="cherokee5c_loss-305-0.119"
+
 TEXT="ᎼᏏ ᎢᎬᏱᏱ ᎤᏬᏪᎳᏅᎯ. ᎠᏯᏙᎸᎢ ᏌᏉ. ᏗᏓᎴᏂᏍᎬᎢ ᎤᏁᎳᏅᎯ ᏚᏬᏢᏁ ᎦᎸᎶᎢ ᎠᎴ ᎡᎶᎯ.";
 
-./tts.sh --checkpoint cherokee5c_loss-245-0.119 --text "$TEXT" --voice 10-chr --griffin_lim --wav 01.wav
-./tts.sh --checkpoint cherokee5c_loss-245-0.119 --text "$TEXT" --voice 04-chr --griffin_lim --wav 02.wav
-./tts.sh --checkpoint cherokee5c_loss-245-0.119 --text "$TEXT" --voice 10-chr --wav 03.wav
+./tts.sh --checkpoint $CP --text "$TEXT" --voice 10-chr --griffin_lim --wav 01.wav
+./tts.sh --checkpoint $CP --text "$TEXT" --voice 04-chr --griffin_lim --wav 02.wav
+./tts.sh --checkpoint $CP --text "$TEXT" --voice 10-chr --wav 03.wav
