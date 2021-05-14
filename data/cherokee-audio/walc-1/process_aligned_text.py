0562b19c49be5765b194d6118362711a330694ee
Michael Conrad <m.conrad.202@gmail.com>
2021-05-14 19:19:58 -0400

    We are Learning Cherokee audio

--- data/cherokee-audio/walc-1/process_aligned_text.py
+++ data/cherokee-audio/walc-1/process_aligned_text.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
+import re
+
+import json
+import os
+import sys
+import numpy as np
+from sklearn.cluster import AffinityPropagation
+from fuzzy_match import algorithims
 
 if __name__ == "__main__":
 
-    import os
-    import sys
-    import unicodedata as ud
-    from chrutils import syl2latin
-    
-    """
-        exclude_syllabary can only be True if do_transliterate is True!
-    """
-    
-    do_transliterate:bool = True
-    exclude_syllabary:bool = False
-    
-    aligned_text: str = "text-copyrighted/aligned-text.txt"
-    output_text: str = "thirteen-moons-selected.txt"
+    voice_ranges: dict = dict()
+    if os.path.exists("voice-ranges.json"):
+        with open("voice-ranges.json", "r") as f:
+            for (range_start, voice_id) in json.loads(f.read())["ranges"]:
+                voice_ranges[int(range_start)] = voice_id
+
+    print(voice_ranges)
+
+    aligned_text: str = "aligned.txt"
+    output_text: str = "selected.txt"
+    training1_text: str = "training1.txt"
+    training2_text: str = "training2.txt"
 
     workdir: str = os.path.dirname(sys.argv[0])
     if workdir.strip() != "":
         os.chdir(workdir)
     workdir = os.getcwd()
 
-    from os import walk
-
     mp3s: list = []
     for (dirpath, dirnames, filenames) in os.walk("mp3"):
         mp3s.extend(filenames)
-        break
     mp3s.sort()
     print(f"Loaded {len(mp3s):,} MP3s")
 
@@ -39,29 +42,80 @@ if __name__ == "__main__":
             lines.append(line.strip())
     print(f"Loaded {len(lines):,} text aligments")
 
+    entries: list = list()
     count: int = 0
+
+    line_no: int = 0
+    mp3: str
+    line: str
+    voice: str = "?"
+    for mp3, line in zip(mp3s, lines):
+        line_no += 1
+        if line_no in voice_ranges.keys():
+            voice = voice_ranges[line_no]
+        line = line.strip()
+        mp3 = mp3.replace("mp3/", "")
+        if "|" in line:
+            parts: list = line.split("|")
+            if len(parts) != 2:
+                continue
+            if parts[1].strip() != mp3:
+                raise Exception(f"Alignment fail [{line_no}] mp3={mp3} marker={parts[1]} text={parts[0]}")
+        if "x" in line.lower():
+            continue
+        entries.append((voice, os.path.join("mp3", mp3), line))
+        count += 1
+
     with open(output_text, "w") as f:
-        line_no: int = 0
+        voice: str
         mp3: str
         line: str
-        for mp3, line in zip(mp3s, lines):
-            line_no += 1
-            line = line.strip()
-            mp3 = mp3.replace("mp3/", "")
-            if "|" in line:
-                parts: list = line.split("|")
-                if len(parts) != 2:
-                    continue
-                if parts[1].strip() != mp3:
-                    raise Exception(f"Alignment fail [{line_no}] mp3={mp3} marker={parts[1]} text={parts[0]}")
-            if "x" in line.lower():
-                continue
-            if not exclude_syllabary:
-                print(f"?|mp3/{mp3}|{line}", file=f)
-                count += 1
-            if do_transliterate:
-                line=syl2latin(line)
-                print(f"?|mp3/{mp3}|{line}", file=f)
-                count += 1
+        for (voice, mp3, line) in entries:
+            print(f"{voice}|{mp3}|{line}", file=f)
 
-    print(f"Output {count:,} {aligned_text} entries")
+    prev = ""
+    pset: int = 0
+    line_no = 0
+    with open(training1_text, "w") as t1:
+        with open(training2_text, "w") as t2:
+            voice: str
+            mp3: str
+            line: str
+            print("ID|PSET|DIALOG|PRONOUN|VERB|GENDER|SYLLABARY|PRONOUNCE|ENGLISH|AUDIO FILE", file=t1)
+            print("ID|PSET|DIALOG|PRONOUN|VERB|GENDER|SYLLABARY|PRONOUNCE|ENGLISH|AUDIO FILE", file=t2)
+            buckets: dict = dict([])
+            voice: str
+            mp3: str
+            text: str
+            for (voice, mp3, text) in entries:
+                text = text.lower().strip()
+                text = re.sub("(?i)[^a-z ]", "", text)
+                text = re.sub("\\s+", " ", text)
+                gender: str = ""
+                if "-f-" in voice:
+                    gender = "female"
+                if "-m-" in voice:
+                    gender = "male"
+                d = algorithims.trigram(prev, text)
+                prev = text
+                if d < 0.17:
+                    pset += 1
+                    buckets[pset] = list()
+                line_no += 1
+                text = f"{line_no:04d}|{pset}||||{gender}||{text}||{mp3}"
+                buckets[pset].append(text)
+                print(text, file=t1)
+            keep_going: bool = True
+            ix: int = 0
+            while keep_going:
+                keep_going = False
+                for bucket in buckets.keys():
+                    if len(buckets[bucket]) > ix:
+                        keep_going = True
+                        print(f"{buckets[bucket][ix]}", file=t2)
+                ix += 1
+
+    print(f"Output {len(entries):,} {aligned_text} TTS training entries")
+
+
+
--- data/cherokee-audio/walc-1/split_audio.py
+++ data/cherokee-audio/walc-1/split_audio.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-
+import pydub.effects
 from pydub import AudioSegment
+from os import walk
+
+from pydub import effects
 
 
 def detect_sound(sound: AudioSegment, silence_threshold: float = -55.0, chunk_size: int = 150) -> list:
@@ -15,9 +18,10 @@ def detect_sound(sound: AudioSegment, silence_threshold: float = -55.0, chunk_si
     sound_start: int = -1
     segments: list = list()
 
+    segment_mid: int = len(sound)
     for position in range(0, len(sound), 10):  # process in 10 ms chunks
         segment_end: int = min(position + chunk_size, len(sound))
-        segment_mid: int = min(position + chunk_size / 2, len(sound))
+        segment_mid = min(int(position + chunk_size / 2), len(sound))
         nextChunk: AudioSegment = sound[position:segment_end]
         if sound_start < 0 and nextChunk.dBFS <= silence_threshold:
             continue
@@ -41,7 +45,7 @@ def detect_sound(sound: AudioSegment, silence_threshold: float = -55.0, chunk_si
     return segments
 
 
-def combine_segments(segments: list, target_length: int) -> list:
+def combine_segments(segments: list, gap_break_duration: int, target_length: int) -> list:
     new_segments: list = list()
 
     if len(segments) == 0:
@@ -51,18 +55,20 @@ def combine_segments(segments: list, target_length: int) -> list:
         new_segments.append(segments[0])
         return new_segments
 
-    start = segments[0][0]
-    end = segments[0][1]
+    (start, end) = segments[0]
 
+    this_start: int
+    this_end: int
     for ix in range(1, len(segments)):
-        if segments[ix][1] - start < target_length:
-            end = segments[ix][1]
+        (this_start, this_end) = segments[ix]
+        if this_start - end < gap_break_duration and this_end - start < target_length:
+            end = this_end
             continue
         new_segments.append((start, end))
-        start = segments[ix][0]
-        end = segments[ix][1]
+        start = this_start
+        end = this_end
 
-    if new_segments[-1][0] != start:
+    if not new_segments or start != new_segments[-1]:
         new_segments.append((start, end))
 
     return new_segments
@@ -76,9 +82,10 @@ if __name__ == "__main__":
     from builtins import list
     from pydub.effects import normalize
 
-    silence_threshold: float = -50.0
-    silence_min_duration: int = 125  # ms
+    silence_threshold: float = -40.0
+    silence_min_duration: int = 250  # 225  # ms
     max_target_duration: int = 10000  # ms
+    gap_break_duration: int = 750  # 675  # ms
 
     workdir: str = os.path.dirname(sys.argv[0])
     if workdir.strip() != "":
@@ -89,46 +96,52 @@ if __name__ == "__main__":
     rmtree("mp3", ignore_errors=True)
     os.mkdir("mp3")
 
-    from os import walk
-
-    mp3s = []
+    audio_files = []
     for (dirpath, dirnames, filenames) in walk("src"):
-        mp3s.extend(filenames)
-        break
-    mp3s.sort()
+        for filename in filenames:
+            audio_files.append(os.path.join(dirpath, filename))
+    print(f"Found {len(audio_files):,} audio files for processing")
+    audio_files.sort()
     splits: list = []
     tix: int = 1
     min_length: float = -1.0
     max_length: float = 0.0
     total_length: float = 0.0
-    for mp3 in mp3s:
-        if os.path.splitext(mp3)[1].lower() != ".mp3":
+    for audio_file in audio_files:
+        ext: str = os.path.splitext(audio_file)[1].lower()
+        if ext not in [".mp3", ".wav"]:
             continue
-        print(f"Processing {mp3}")
 
-        mp3 = os.path.splitext(mp3)[0]
-        data = AudioSegment.from_mp3("src/" + mp3 + ".mp3").set_channels(1)
+        data: AudioSegment = AudioSegment.from_file(audio_file).set_channels(1)
+        data = effects.low_pass_filter(data, 14000)
+        data = effects.high_pass_filter(data, 350)
+        data = normalize(data)
 
-        print(f" - segment hunting")
         segments = detect_sound(data, silence_threshold, silence_min_duration)
-
-        segments = combine_segments(segments, max_target_duration)
+        segments = combine_segments(segments, gap_break_duration, max_target_duration)
 
         if len(segments) == 0:
-            print(f"=== ONLY SILENCE FOUND IN: {mp3}")
+            print(f"=== ONLY SILENCE FOUND IN: {audio_file}")
             continue
 
         for segment_start, segment_end in segments:
-            chunk_mp3 = f"{tix:04d}-{mp3}"
-            tix += 1
+            if "/" in audio_file:
+                audio_file = audio_file[audio_file.rfind("/") + 1:]
+
             # Normalize the chunk.
             normalized: AudioSegment = normalize(data[segment_start:segment_end])
 
-            # duration = len(normalized)
+            # If it is very short, it's probably just noise, skip it
+            if normalized.duration_seconds < 0.25:
+                continue
 
-            print(f"Saving mp3/{chunk_mp3}-{int(segment_start):06d}.mp3.")
-            normalized.export(f"mp3/{chunk_mp3}-{int(segment_start):06d}.mp3", bitrate="192k", format="mp3")
-            splits.append(f"mp3/{chunk_mp3}-{int(segment_start):06d}.mp3")
+            chunk_mp3 = f"{tix:04d}-{audio_file}".replace("/", "_").replace(" ", "_").replace("__", "_")
+            chunk_mp3 = os.path.splitext(chunk_mp3)[0]
+            output_file = f"mp3/{chunk_mp3}-{int(segment_start):06d}.mp3"
+            tix += 1
+            print(f"Saving {output_file}.")
+            normalized.export(output_file, format="mp3", parameters=["-qscale:a", "0"])
+            splits.append(output_file)
 
             duration: float = normalized.duration_seconds
             total_length += duration
@@ -137,24 +150,18 @@ if __name__ == "__main__":
             if max_length < duration:
                 max_length = duration
 
-    with open("split_audio.txt", "w") as f:
-        for mp3 in splits:
-            if os.path.splitext(mp3)[1].lower() != ".mp3":
-                continue
-            f.write("?")  # speaker
-            f.write("|")
-            f.write(mp3)  # audio file
-            f.write("|")
-            f.write("\n")
+    msg: str = "\n"
+    msg += f"Work directory: {workdir}\n"
+    msg += "\n"
+    msg += f"Total splits: {len(splits):,}\n"
+    msg += "\n"
+    msg += f"Min duration: {min_length:,.02f} seconds\n"
+    msg += f"Max duration: {max_length:,.02f} seconds\n"
+    msg += f"Total duration: {total_length:,.02f} seconds\n"
+    msg += "\n"
 
-    print()
-    print(f"Work directory: {workdir}")
-    print()
-    print(f"Total splits: {len(splits):,}")
-    print()
-    print(f"Min duration: {min_length:,.02f} seconds")
-    print(f"Max duration: {max_length:,.02f} seconds")
-    print(f"Total duration: {total_length:,.02f} seconds")
-    print()
+    with open("split_audio-stats.txt", "w") as f:
+        f.write(msg)
+    print(msg)
 
     sys.exit()
