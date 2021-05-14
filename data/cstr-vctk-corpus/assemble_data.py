0af1f547365e236f9c9a16fb0546e30c1237e04e
Michael Conrad <m.conrad.202@gmail.com>
2021-05-14 19:17:33 -0400

    Multi voice English data

--- data/cstr-vctk-corpus/assemble_data.py
+++ data/cstr-vctk-corpus/assemble_data.py
@@ -1,154 +1,155 @@
 #!/usr/bin/env python3
 import os
-import sys
-import string
-import unicodedata as ud
-import random
-import re
 import pathlib
-import subprocess
-from shutil import rmtree
-from pydub import AudioSegment
-import pydub.effects as effects
-from split_audio import detect_sound
+import re
+import sys
+import unicodedata as ud
+import warnings
 from builtins import list
+from os import listdir
+from os.path import isdir
+from os.path import isfile
+from os.path import join
+from shutil import rmtree
 
-include_o_form:bool = True
+import progressbar
+import pydub.effects as effects
+import torch.hub
+from pydub import AudioSegment
 
 if __name__ == "__main__":
 
-    if (sys.argv[0].strip()!=""):
+    warnings.filterwarnings("ignore")
+
+    diaz_pipeline = torch.hub.load('pyannote/pyannote-audio', 'dia', device="cpu")
+
+    skip_speakers: set = set()  # speakers to skip due to audio quality issues (hums, etc)
+    skip_speakers.add("236-en")
+
+    max_length: float = 10.0
+
+    if sys.argv[0].strip() != "":
         os.chdir(os.path.dirname(sys.argv[0]))
-    
-    max_duration:float=10.0
-    MASTER_TEXTS:list=["aligned.txt"]
-    
-    use_augmented:bool=False
-    if use_augmented:
-        MASTER_TEXTS.append("augmented.txt")
-    
-    #cleanup any previous runs
-    for dir in ["linear_spectrograms", "spectrograms", "wav"]:
-        rmtree(dir, ignore_errors=True)
-        
-    pathlib.Path(".").joinpath("wav").mkdir(exist_ok=True)
-    
-    entries: dict = {}
-    for MASTER_TEXT in MASTER_TEXTS:
-        with open(MASTER_TEXT, "r") as f:
-            for line in f:
-                fields=line.split("|")
-                speaker: str=fields[0].strip()
-                mp3: str=fields[1].strip()
-                text: str=ud.normalize("NFD", fields[2].strip())
-                dedupeKey=speaker+"|"+text+"|"+mp3
-                if text=="" or "XXX" in text:
-                    continue
-                if not include_o_form and "\u030a" in text:
-                    continue
-                entries[dedupeKey]=(speaker,mp3,text)
-    
-    print(f"Loaded {len(entries):,} entries with audio and text.")
-    
-    #the voice id to use for any "?" marked entries
-    voiceid:str=""
-    with open("voice-id.txt", "r") as f:
-        for line in f:
-            voiceid=line.strip()
-            break
-        
-    #to map any non "?" marked entries from annotation short hand id to ML assigned sequence id
-    voiceids:dict = {}
-    with open("../voice-ids.txt") as f:
-        for line in f:
-            mapping=line.strip()
-            fields = mapping.split("|")
-            if len(fields)<2 or fields[1].strip() == "":
-                break
-            id=fields[0].strip()
-            if id.strip()=="":
+
+    max_duration: float = 10.0
+    output_text: str = "cstr-vctk-corpus.txt"
+
+    # cleanup any previous runs
+    for folder in ["mp3"]:
+        rmtree(folder, ignore_errors=True)
+    pathlib.Path(".").joinpath("mp3").mkdir(exist_ok=True)
+
+    txt_dirs: list = [d for d in listdir("txt") if isdir(join("txt", d))]
+    txt_dirs.sort()
+
+    entries: list = list()
+
+    for d in txt_dirs:
+        txt_files: list = [f for f in listdir(join("txt", d)) if isfile(join("txt", d, f))]
+        txt_files.sort()
+        for txt_file in txt_files:
+            speaker = re.sub(".*?(\\d+)_.*", "\\1", txt_file) + "-en"
+            if speaker in skip_speakers:
                 continue
-            for field in fields[1:]:
-                if field.strip() == "":
-                    continue
-                voiceids[field.strip()]=id
-    
-    id:int=1
-    
-    shortestLength:float=-1
-    longestLength:float=0.0
-    totalLength:float=0.0
-    print("Creating wavs")
-    rows:list=[]
-    for speaker, mp3, text in entries.values():
-        wav:str="wav/"+os.path.splitext(os.path.basename(mp3))[0]+".wav"
-        text:str=ud.normalize('NFD', text)
-        mp3_segment:AudioSegment=AudioSegment.from_file(mp3)
-        segments:list = detect_sound(mp3_segment)
-        if len(segments) > 1:
-            mp3_segment=mp3_segment[segments[0][0]:segments[-1][1]]
-        if mp3_segment.duration_seconds > max_duration:
+            mp3_file = join("mp3", txt_file.replace(".txt", ".mp3"))
+            wav_file = join("wav48", d, txt_file.replace(".txt", ".wav"))
+            txt_file = join("txt", d, txt_file)
+            text: str
+            with open(txt_file, "r") as f:
+                for line in f:
+                    text = line.strip().replace("|", " ")
+                    break
+            entries.append((wav_file, speaker, mp3_file, text))
+
+    print(f"Loaded {len(entries):,} entries.")
+
+    bar = progressbar.ProgressBar(maxval=len(entries))
+    bar.start()
+    idx: int = 0
+    count: int = 0
+    lang: str = "en"
+    shortestLength: float = -1
+    longestLength: float = 0.0
+    totalLength: float = 0.0
+    print("Creating mp3s")
+    rows: list = []
+    for wav, speaker, mp3, text in entries:
+        bar.update(count)
+        count += 1
+        text: str = ud.normalize('NFD', text)
+        wav_segment: AudioSegment = AudioSegment.from_file(wav)
+        wav_segment = effects.normalize(wav_segment)
+
+        diaz_start: int = len(wav_segment)
+        diaz_end: int = 0
+        diaz = diaz_pipeline({"audio": wav})
+        for turn, _, _ in diaz.itertracks(yield_label=True):
+            ts = int(turn.start * 1000 - 10)
+            if ts < 0:
+                ts = 0
+            te = int(turn.end * 1000 + 10)
+            if te > len(wav_segment):
+                te = len(wav_segment)
+            if ts < diaz_start:
+                diaz_start = ts
+            if te > diaz_end:
+                diaz_end = te
+
+        if diaz_end == 0 and diaz_start == len(wav_segment):
             continue
-        audio:AudioSegment = AudioSegment.silent(125, 22050)
-        audio = audio.append(mp3_segment, crossfade=0)
-        audio = audio.append(AudioSegment.silent(125, 22050))
+
+        if diaz_end - diaz_start < 500:
+            diaz_start = 0
+            diaz_end = len(wav_segment)
+
+        audio: AudioSegment = wav_segment[diaz_start:diaz_end]
         audio = effects.normalize(audio)
         audio = audio.set_channels(1)
         audio = audio.set_frame_rate(22050)
-        audio.export(wav, format="wav")
-        totalLength+=audio.duration_seconds
+        audio.export(mp3, format="mp3", parameters=["-q:a", "3"])
+        totalLength += audio.duration_seconds
         if shortestLength < 0 or shortestLength > audio.duration_seconds:
             shortestLength = audio.duration_seconds
         if longestLength < audio.duration_seconds:
             longestLength = audio.duration_seconds
-        vid:str=speaker
-        if vid in voiceids.keys():
-            vid = voiceids[vid]
-        if vid=="?":
-            vid=voiceid        
-        rows.append(f"{id:06d}|{vid}|chr|{wav}|||{text}|")
-        id+=1
-    
-    totalLength=int(totalLength)
-    minutes=int(totalLength/60)
-    seconds=int(totalLength%60)
-    print(f"Total duration: {minutes:,}:{seconds:02}")
-    
-    shortestLength=int(shortestLength)
-    minutes=int(shortestLength/60)
-    seconds=int(shortestLength%60)
-    print(f"Shortest duration: {minutes:,}:{seconds:02}")
-    
-    longestLength=int(longestLength)
-    minutes=int(longestLength/60)
-    seconds=int(longestLength%60)
-    print(f"Longest duration: {minutes:,}:{seconds:02}")
-    
-    print("Creating training files")
-    #save all copy before shuffling
-    with open("all.txt", "w") as f:
+        idx += 1
+        rows.append(f"{idx:06d}|{speaker}|{lang}|{mp3}|||{text}|")
+
+    bar.finish()
+
+    with open("assemble-stats.txt", "w") as f:
+        print(f"Output {idx:,} entries.", file=f)
+
+        print(file=f)
+
+        totalLength = int(totalLength)
+        minutes = int(totalLength / 60)
+        seconds = int(totalLength % 60)
+        print(f"Total duration: {minutes:,}:{seconds:02}", file=f)
+
+        shortestLength = int(shortestLength)
+        minutes = int(shortestLength / 60)
+        seconds = int(shortestLength % 60)
+        print(f"Shortest duration: {minutes:,}:{seconds:02}", file=f)
+
+        longestLength = int(longestLength)
+        minutes = int(longestLength / 60)
+        seconds = int(longestLength % 60)
+        print(f"Longest duration: {minutes:,}:{seconds:02}", file=f)
+
+        print(file=f)
+
+    print("Creating final output file")
+    with open(output_text, "w") as f:
         for line in rows:
             f.write(line)
             f.write("\n")
-    
-    #create train/val sets                        
-    random.Random(id).shuffle(rows)
-    trainSize:int=(int)(len(rows)*.95)
-    valSize:int=len(rows)-trainSize
-    
-    with open("train.txt", "w") as f:
-        for line in rows[:trainSize]:
-            f.write(line)
-            f.write("\n")
-            
-    with open("val.txt", "w") as f:
-        for line in rows[trainSize:]:
-            f.write(line)
-            f.write("\n")
-    
-    print(f"Train size: {trainSize}")
-    print(f"Val size: {valSize}")
-    print(f"All size: {len(rows)}")
-    print("Folder:",pathlib.Path(".").resolve().name)        
-    
+
+    with open("assemble-stats.txt", "a") as f:
+        print(f"All size: {len(rows)}", file=f)
+        print(file=f)
+        print("Folder:", pathlib.Path(".").resolve().name, file=f)
+        print(file=f)
+
+    print("done")
     sys.exit()
--- data/cstr-vctk-corpus/generateTrainingData.py
+++ data/cstr-vctk-corpus/generateTrainingData.py
@@ -6,6 +6,7 @@ import random
 import pathlib
 from shutil import rmtree
 
+import progressbar
 from pydub import AudioSegment
 import pydub.effects as effects
 from split_audio import detect_sound
@@ -13,91 +14,110 @@ from builtins import list
 
 if __name__ == "__main__":
 
-    os.chdir(os.path.dirname(sys.argv[0]))
-    
-    MASTER_TEXT:str="comvoi-clean.txt"
-    max_duration:float=10.0
-    
+    if sys.argv[0].strip() != "" and os.path.dirname(sys.argv[0]) != "":
+        os.chdir(os.path.dirname(sys.argv[0]))
+
+    MASTER_TEXT: str = "cstr-vctk-corpus.txt"
+    max_duration: float = 10.0
+    min_duration: float = 0.4
+
     rmtree("wav", ignore_errors=True)
     pathlib.Path(".").joinpath("wav").mkdir(exist_ok=True)
-    langs:set=set()
-    
+    langs: set = set()
+
     with open(MASTER_TEXT, "r") as f:
         entries: dict = {}
         for line in f:
-            fields=line.split("|")
-            xid: str=fields[0].strip()
-            spkr: str=fields[1].strip()
-            lang: str=fields[2].strip()        
-            mp3: str=fields[3].strip()        
-            text: str=ud.normalize("NFD", fields[6].strip())
-            dedupeKey=spkr+"|"+text
-            if text=="":
+            fields = line.split("|")
+            xid: str = fields[0].strip()
+            spkr: str = fields[1].strip()
+            lang: str = fields[2].strip()
+            mp3: str = fields[3].strip()
+            text: str = ud.normalize("NFD", fields[6].strip())
+            dedupeKey = spkr + "|" + text + "|" + mp3
+            if text == "":
                 continue
-            entries[dedupeKey]=(xid,spkr,lang,mp3,text)
+            entries[dedupeKey] = (xid, spkr, lang, mp3, text)
             langs.add(lang)
-    
+
     print(f"Loaded {len(entries):,} entries with audio and text.")
-    
-    shortestLength:float=-1
-    longestLength:float=0.0
-    totalLength:float=0.0
+
+    shortestLength: float = -1
+    longestLength: float = 0.0
+    totalLength: float = 0.0
     print("Creating wavs")
-    rows:list=[]
+
+    bar = progressbar.ProgressBar(maxval=len(entries))
+    bar.start()
+
+    counter: int = 0
+    rows: list = []
     for xid, speaker, lang, mp3, text in entries.values():
-        wav:str="wav/"+os.path.splitext(os.path.basename(mp3))[0]+".wav"
-        text:str=ud.normalize('NFD', text)
-        mp3_segment:AudioSegment=AudioSegment.from_file(mp3)
-        segments:list = detect_sound(mp3_segment)
+        bar.update(counter)
+        counter += 1
+        wav: str = "wav/" + os.path.splitext(os.path.basename(mp3))[0] + ".wav"
+        text: str = ud.normalize('NFD', text)
+        mp3_segment: AudioSegment = AudioSegment.from_file(mp3)
+        segments: list = detect_sound(mp3_segment)
         if len(segments) > 1:
-            mp3_segment=mp3_segment[segments[0][0]:segments[-1][1]]
+            mp3_segment = mp3_segment[segments[0][0]:segments[-1][1]]
         if mp3_segment.duration_seconds > max_duration:
             continue
-        audio:AudioSegment = AudioSegment.silent(125, 22050)
+        if mp3_segment.duration_seconds < min_duration:
+            continue
+        audio: AudioSegment = AudioSegment.silent(125, 22050)
         audio = audio.append(mp3_segment, crossfade=0)
-        audio = audio.append(AudioSegment.silent(125, 22050))
+        audio = audio.append(AudioSegment.silent(125, 22050), crossfade=0)
         audio = effects.normalize(audio)
         audio = audio.set_channels(1)
         audio = audio.set_frame_rate(22050)
         audio.export(wav, format="wav")
-        totalLength+=audio.duration_seconds
+        totalLength += audio.duration_seconds
         if shortestLength < 0 or shortestLength > audio.duration_seconds:
             shortestLength = audio.duration_seconds
         if longestLength < audio.duration_seconds:
             longestLength = audio.duration_seconds
         rows.append(f"{xid}|{speaker}|{lang}|{wav}|||{text}|")
-    
-    totalLength=int(totalLength)
-    minutes=int(totalLength/60)
-    seconds=int(totalLength%60)
-    print(f"Total duration: {minutes:,}:{seconds:02}")
-    
-    shortestLength=int(shortestLength)
-    minutes=int(shortestLength/60)
-    seconds=int(shortestLength%60)
-    print(f"Shortest duration: {minutes:,}:{seconds:02}")
-    
-    longestLength=int(longestLength)
-    minutes=int(longestLength/60)
-    seconds=int(longestLength%60)
-    print(f"Longest duration: {minutes:,}:{seconds:02}")
-    
+
+    with open("stats.txt", "w") as f:
+        print(f"Output {len(rows):,} entries.", file=f)
+
+        print(file=f)
+
+        totalLength = int(totalLength)
+        hours = int(totalLength / 3600)
+        minutes = int(totalLength % 3600 / 60)
+        seconds = int(totalLength % 60)
+        print(f"Total duration: {hours:,}:{minutes:02}:{seconds:02}", file=f)
+
+        shortestLength = int(shortestLength)
+        minutes = int(shortestLength / 60)
+        seconds = int(shortestLength % 60)
+        print(f"Shortest duration: {minutes:,}:{seconds:02}", file=f)
+
+        longestLength = int(longestLength)
+        minutes = int(longestLength / 60)
+        seconds = int(longestLength % 60)
+        print(f"Longest duration: {minutes:,}:{seconds:02}", file=f)
+
+        print(file=f)
+
     print("Creating training files")
-    
-    #save all copy before shuffling
+
+    # sort then save all copy before shuffling
+    rows.sort()
     with open("all.txt", "w") as f:
         for line in rows:
             f.write(line)
             f.write("\n")
-            
+
     with open("train.txt", "w") as f:
         f.write("")
     with open("val.txt", "w") as f:
         f.write("")
-        
-            
+
     for lang in langs:
-        subset:list=list()
+        subset: list = list()
         for row in rows:
             rlang = row.split("|")[2]
             if rlang != lang:
@@ -105,22 +125,25 @@ if __name__ == "__main__":
             subset.append(row)
         print(f" {lang} length: {len(subset):,}")
         random.Random(len(subset)).shuffle(subset)
-        #create train/val sets - splitting up data by language evenly
-        trainSize:int=(int)(len(subset)*.95)
-        valSize:int=len(subset)-trainSize
+        # create train/val sets - splitting up data by language evenly
+        trainSize: int = (int)(len(subset) * .95)
+        valSize: int = len(subset) - trainSize
         with open("train.txt", "a") as f:
             for line in subset[:trainSize]:
                 f.write(line)
                 f.write("\n")
-                
+
         with open("val.txt", "a") as f:
             for line in subset[trainSize:]:
                 f.write(line)
                 f.write("\n")
-        print(f"  Train size for {lang}: {trainSize}")
-        print(f"   Val size for {lang}: {valSize}")
-    
-    print(f"All size: {len(rows)}")
-    print("Folder:",pathlib.Path(".").resolve().name)        
-    
+
+    with open("stats.txt", "a") as f:
+        print(f"All size: {len(rows)}", file=f)
+        print(f"Train size: {trainSize}", file=f)
+        print(f"Val size: {valSize}", file=f)
+        print(file=f)
+        print("Folder:", pathlib.Path(".").resolve().name, file=f)
+        print(file=f)
+
     sys.exit()
