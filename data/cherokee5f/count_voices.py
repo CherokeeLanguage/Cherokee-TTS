2fa77a746ac695e99c4015fabe8460185b748527
Michael Conrad <m.conrad.202@gmail.com>
2021-05-14 19:28:38 -0400

    Generate stats

--- data/cherokee5f/count_voices.py
+++ data/cherokee5f/count_voices.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from typing import Set
 
 import os
 import sys
@@ -9,44 +8,33 @@ import json
 from shutil import rmtree
 import pathlib
 import re
-from typing import Dict
 
 if __name__ == "__main__":
-
-    workdir: str = os.path.dirname(sys.argv[0])
+    
+    workdir:str = os.path.dirname(sys.argv[0])
     if workdir.strip() != "":
         os.chdir(workdir)
     workdir = os.getcwd()
-
-    voice_counts: dict = dict()
-    voice_counts_by_lang: Dict[str, Set] = dict()
-
+    
+    voice_counts:dict = dict()
+    
     with open("all.txt", "r") as f:
         for line in f:
             fields = line.split("|")
             if len(fields) < 2:
                 continue
-            voice: str = fields[1]
-            if voice not in voice_counts.keys():
-                voice_counts[voice] = 0
+            voice = fields[1]
+            if not voice in voice_counts.keys():
+                voice_counts[voice]=0
             voice_counts[voice] = voice_counts[voice] + 1
-            lang: str = fields[2]
-            if lang not in voice_counts_by_lang.keys():
-                voice_counts_by_lang[lang] = set()
-            voice_counts_by_lang[lang].add(voice)
-
-    with open("voice-counts-by-language.txt", "w") as f:
-        for lang in sorted(list(voice_counts_by_lang.keys())):
-            count: int = len(voice_counts_by_lang[lang])
-            print(f"{lang}:{count:,}", file=f)
-
-    counts_list: list = list()
+            
+    counts_list:list = list()
     for key in voice_counts.keys():
         counts_list.append((key, voice_counts[key]))
-
+    
     counts_list.sort(key=lambda value: value[1], reverse=True)
-
-    counter: int = 0
+    
+    counter:int = 0
     for idx in range(len(counts_list)):
         count = counts_list[idx]
         if "cno-" in count[0] or "-chr" in count[0]:
@@ -56,7 +44,7 @@ if __name__ == "__main__":
         if counter >= 3:
             break
 
-    voices: set = set()
+    voices:set = set()
     with open("train.txt", "r") as f:
         for line in f:
             fields = line.split("|")
@@ -67,7 +55,7 @@ if __name__ == "__main__":
 
     with open("voices.inc", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             f.write("\"")
@@ -78,7 +66,7 @@ if __name__ == "__main__":
 
     with open("voices.txt", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             f.write(voice)
@@ -86,7 +74,7 @@ if __name__ == "__main__":
 
     with open("voices-xchr.inc", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             if "-chr" in voice:
@@ -99,7 +87,7 @@ if __name__ == "__main__":
 
     with open("voices-xchr.txt", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             if "-chr" in voice:
@@ -109,7 +97,7 @@ if __name__ == "__main__":
 
     with open("voices-chr.inc", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             if not "-chr" in voice:
@@ -122,10 +110,13 @@ if __name__ == "__main__":
 
     with open("voices-chr.txt", "w") as f:
         for voice_count in counts_list:
-            voice: str = voice_count[0]
+            voice:str = voice_count[0]
             if not voice in voices:
                 continue
             if not "-chr" in voice:
                 continue
             f.write(voice)
             f.write("\n")
+        
+        
+                                              
--- data/cherokee5g/count_langs.py
+++ data/cherokee5g/count_langs.py
@@ -16,107 +16,46 @@ if __name__ == "__main__":
         os.chdir(workdir)
     workdir = os.getcwd()
     
-    voice_counts:dict = dict()
+    lang_counts:dict = dict()
     
     with open("all.txt", "r") as f:
         for line in f:
             fields = line.split("|")
             if len(fields) < 2:
                 continue
-            voice = fields[1]
-            if not voice in voice_counts.keys():
-                voice_counts[voice]=0
-            voice_counts[voice] = voice_counts[voice] + 1
+            lang = fields[2]
+            if not lang in lang_counts.keys():
+                lang_counts[lang]=0
+            lang_counts[lang] = lang_counts[lang] + 1
             
     counts_list:list = list()
-    for key in voice_counts.keys():
-        counts_list.append((key, voice_counts[key]))
+    for key in lang_counts.keys():
+        counts_list.append((key, lang_counts[key]))
     
     counts_list.sort(key=lambda value: value[1], reverse=True)
     
     counter:int = 0
     for idx in range(len(counts_list)):
         count = counts_list[idx]
-        if "cno-" in count[0] or "-chr" in count[0]:
-            continue
         print(count)
         counter += 1
-        if counter >= 3:
-            break
 
-    voices:set = set()
+    langs:set = set()
     with open("train.txt", "r") as f:
         for line in f:
             fields = line.split("|")
-            voice = fields[1]
-            if voice in voices:
+            lang = fields[2]
+            if lang in langs:
                 continue
-            voices.add(voice)
+            langs.add(lang)
 
-    with open("voices.inc", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
+    with open("langs.txt", "w") as f:
+        for lang_count in counts_list:
+            lang:str = lang_count[0]
+            if not lang in langs:
                 continue
-            f.write("\"")
-            f.write(voice)
-            f.write("\"")
-            f.write(" ")
-        f.write("\n")
-
-    with open("voices.txt", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
-                continue
-            f.write(voice)
+            f.write(f"{lang_count[0]}: {int(lang_count[1]):,}")
             f.write("\n")
 
-    with open("voices-xchr.inc", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
-                continue
-            if "-chr" in voice:
-                continue
-            f.write("\"")
-            f.write(voice)
-            f.write("\"")
-            f.write(" ")
-        f.write("\n")
-
-    with open("voices-xchr.txt", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
-                continue
-            if "-chr" in voice:
-                continue
-            f.write(voice)
-            f.write("\n")
-
-    with open("voices-chr.inc", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
-                continue
-            if not "-chr" in voice:
-                continue
-            f.write("\"")
-            f.write(voice)
-            f.write("\"")
-            f.write(" ")
-        f.write("\n")
-
-    with open("voices-chr.txt", "w") as f:
-        for voice_count in counts_list:
-            voice:str = voice_count[0]
-            if not voice in voices:
-                continue
-            if not "-chr" in voice:
-                continue
-            f.write(voice)
-            f.write("\n")
-        
         
                                               
