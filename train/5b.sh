f2fb818dd5abb41fc715ee1604cd754314b46541
Michael Conrad <m.conrad.202@gmail.com>
2021-05-16 08:55:47 -0400

    refactor dir structure

---
+++ train/5b.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -i
+set -e
+set -o pipefail
+clear
+
+conda activate Cherokee-TTS
+
+params="5a"
+
+cd "$(dirname "$0")"
+WORK="$(pwd)"
+
+cd data
+python prepare_spectrograms.py --directory "$params"
+
+date
+
+cd "$WORK"
+export PYTHONIOENCODING=utf-8
+python trainGa.py --hyper_parameters "$params" --accumulation_size 3
+
+date
