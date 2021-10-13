a190baac083a3ede0ba2395f883361c4682bfbdb
Michael Conrad <m.conrad.202@gmail.com>
2021-10-13 17:37:58 -0400

    add missing script file

---
+++ train/5f.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -i
+set -e
+set -o pipefail
+
+params="5f"
+
+cd "$(dirname "$0")/.."
+WORK="$(pwd)"
+
+export PYTHONIOENCODING=utf-8
+conda activate Cherokee-TTS
+
+clear
+
+cd "$WORK"/data
+python prepare_spectrograms.py --directory "datasets/$params"
+
+date
+
+cd "$WORK"
+python train.py --hyper_parameters "$params"
+
+date
