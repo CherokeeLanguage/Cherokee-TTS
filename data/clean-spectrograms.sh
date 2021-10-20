b419456c2cdcc5852423384415fa5153ab440285
Michael Conrad <m.conrad.202@gmail.com>
2021-10-20 17:03:53 -0400

    fix typo

--- data/clean-spectrograms.sh
+++ data/clean-spectrograms.sh
@@ -2,12 +2,14 @@
 
 cd "$(dirname "$0")" || exit 1
 
-find . -name lin_spectograms | while read -r d; do
+find . -name lin_spectrograms | while read -r d; do
   echo "$d"
   rm -r "$d"
 done
 
-find . -name mel_spectograms | while read -r d; do
+find . -name mel_spectrograms | while read -r d; do
   echo "$d"
   rm -r "$d"
 done
+
+exit 0
