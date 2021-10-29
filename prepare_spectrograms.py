a5b4065a65adc2299cf586dd4fdd434d7494b1e9
Michael Conrad <m.conrad.202@gmail.com>
2021-10-29 14:02:23 -0400

    save out altered audio for review.

--- prepare_spectrograms.py
+++ prepare_spectrograms.py
@@ -1,8 +1,10 @@
+import shutil
 import unicodedata as ud
 
 import argparse
 import numpy as np
 import os
+import progressbar
 import sys
 from numpy import array
 from pydub import AudioSegment
@@ -14,9 +16,10 @@ from utils import audio
 
 
 def trim_silence(audio_segment: AudioSegment) -> AudioSegment:
+    silence_threshold: float = -40
 
     def trim_leading_silence(tmp_audio: AudioSegment):
-        return tmp_audio[detect_leading_silence(tmp_audio):]
+        return tmp_audio[detect_leading_silence(tmp_audio, silence_threshold=silence_threshold):]
 
     def trim_trailing_silence(tmp_audio: AudioSegment):
         tmp_reversed: AudioSegment = tmp_audio.reverse()
@@ -34,29 +37,21 @@ def main():
     os.chdir("data")
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--directory", type=str, default="datasets/1a",
-                        help="Directory for Training Data (train.txt, val.txt) and Spectrogram Storage.")
-    parser.add_argument("--sample_rate", type=int, default=22050, help="Sample rate.")
-    parser.add_argument("--num_fft", type=int, default=1102, help="Number of FFT frequencies.")
-    parser.add_argument("--num_mels", type=int, default=80, help="Number of mel bins.")
-    parser.add_argument("--stft_window_ms", type=float, default=50, help="STFT window size.")
-    parser.add_argument("--stft_shift_ms", type=float, default=12.5, help="STFT window shift.")
-    parser.add_argument("--no_preemphasis", action='store_false', help="Do not use preemphasis.")
-    parser.add_argument("--preemphasis", type=float, default=0.97, help="Strength of preemphasis.")
+    parser.add_argument("--dataset", type=str, default="1a",
+                        help="Params dataset for Training Data.")
     parser.add_argument("--pad", type=bool, default=True, help="Pad audio with silence.")
 
     args = parser.parse_args()
+    params.load(f"../params/{args.dataset}.json")
 
-    params.sample_rate = args.sample_rate
-    params.num_fft = args.num_fft
+    dataset_path: str = os.path.join("datasets", args.dataset)
+    files_to_solve = [(dataset_path, "train.txt"), (dataset_path, "val.txt"), ]
 
-    files_to_solve = [(args.directory, "train.txt"), (args.directory, "val.txt"), ]
+    if not os.path.exists('mel_spectrograms'):
+        os.makedirs('mel_spectrograms')
 
-    spectrogram_dirs = [os.path.join(args.directory, 'mel_spectrograms')]
-
-    for x in spectrogram_dirs:
-        if not os.path.exists(x):
-            os.makedirs(x)
+    shutil.rmtree("reference-audio", ignore_errors=True)
+    os.makedirs("reference-audio")
 
     metadata = []
     for d, fs in files_to_solve:
@@ -67,7 +62,7 @@ def main():
     print(f'Please wait, this may take a very long time.')
     for d, fs, m in metadata:
         print(f'Creating spectrograms for: {fs}')
-
+        bar: progressbar.ProgressBar = progressbar.ProgressBar(maxval=len(m))
         with open(os.path.join(d, fs + "-tmp"), 'w', encoding='utf-8') as f:
             for i in m:
                 idx, speaker, lang, wav, _, _, raw_text, phonemes = i
@@ -77,16 +72,25 @@ def main():
 
                 py_audio: AudioSegment = AudioSegment.from_file(audio_path)
                 py_audio = py_audio.set_channels(1).set_frame_rate(params.sample_rate)
+                py_audio = effects.normalize(py_audio)
                 py_audio = trim_silence(py_audio)
+
+                # pydub.silence.detect_silence(py_audio, silence_thresh=-40, seek_step=10)
+                # TODO: scan for long silence gaps and either reject the sample or reduce the length of the silence gaps
+
 
                 if args.pad:
                     # Add 100 ms of silence at the beginning, and 150 ms at the end.
                     py_audio = AudioSegment.silent(100) + py_audio + AudioSegment.silent(150)
 
-                py_audio = effects.normalize(py_audio)
+                # Output altered audio (compressed) for manual review
+                mp3_name = f"{lang}_{speaker}-{spec_id:06d}.mp3"
+                ref_audio_mp3: str = os.path.join("reference-audio", mp3_name)
+                if not os.path.exists(ref_audio_mp3):
+                    py_audio.export(ref_audio_mp3, format="mp3", parameters=["-qscale:a", "3"])
+
                 py_audio_samples: array = np.array(py_audio.get_array_of_samples()).astype(np.float32)
                 py_audio_samples = py_audio_samples / (1 << 8 * 2 - 1)
-
                 mel_path_partial = os.path.join("mel_spectrograms", spec_name)
                 mel_path = os.path.join(d, mel_path_partial)
                 if not os.path.exists(mel_path):
@@ -94,10 +98,11 @@ def main():
 
                 raw_text = ud.normalize("NFC", raw_text)
                 phonemes = ud.normalize("NFC", phonemes)
-                print(f'{idx}|{speaker}|{lang}|{wav}|{mel_path_partial}||{raw_text}|{phonemes}', file=f)
 
-                if spec_id % 1000 == 0:
-                    print(f'{idx}|{speaker}|{lang}|{wav}|{mel_path_partial}||{raw_text}|{phonemes}')
+                print(f'{idx}|{speaker}|{lang}|{wav}|{mel_path_partial}||{raw_text}|{phonemes}', file=f)
+                bar.update(bar.currval + 1)
+
+        bar.finish()
 
     for d, fs in files_to_solve:
         tmp = os.path.join(d, fs + "-tmp")
